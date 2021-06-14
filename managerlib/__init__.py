457d71e3c272d8a265d36705bc292850d58314b5
Somnath Pandit <panditsomnath10016@gmail.com>
2021-06-14 17:26:50 +0530

    trying to work on import

--- managerlib/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import managerlib.constants as const
-
-def main():
-    const.initialize_directories()
--- managerlib/constants.py
+++
@@ -1,7 +0,0 @@
-import os
-import datetime as dt
-
-TIMESTAMP = dt.datetime.now()
-
-def initialize_directories():
-    pass
